4b6a0540d642afbf341333e2990009861a6e444d
Gleb <rokkiy@mail.ru>
2023-09-24 20:50:06 +0300

    Initial commit

---
+++ src/main/java/edu/hsai/thermalconductivity1d/App.java
@@ -0,0 +1,16 @@
+package main.java.edu.hsai.thermalconductivity1d;
+
+import static main.java.edu.hsai.thermalconductivity1d.ThermalConductivity.getDistribution;
+
+public class App {
+    public static void main(String[] args) {
+//        int steps = 1000;
+//        int fieldLenght = 16;
+//        double startTemp = 100.0;
+//        double speedCoef = 0.02;
+//        int treadsNum = 16;
+//        getDistribution(steps, fieldLenght, startTemp, speedCoef, treadsNum);
+        getDistribution(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Double.parseDouble(args[1]),
+                Double.parseDouble(args[1]), Integer.parseInt(args[1]));
+    }
+}
---
+++ src/main/java/edu/hsai/thermalconductivity1d/ThermalConductivity.java
@@ -0,0 +1,64 @@
+package main.java.edu.hsai.thermalconductivity1d;
+
+import java.text.DecimalFormat;
+
+public class ThermalConductivity {
+
+    public static double[] getDistribution(int steps, int fieldLenght, double startTemp, double speedCoef, int treadsNum) {
+        DecimalFormat decimalFormat = new DecimalFormat("#.#");
+        double[] temperature = new double[fieldLenght];
+
+        double start = System.currentTimeMillis();
+
+        temperature[fieldLenght / 2 - 1] = startTemp;
+        for (int j = 0; j < fieldLenght; j++) {
+            System.out.printf("%6s ", decimalFormat.format(temperature[j]));
+        }
+        System.out.println();
+
+        Thread[] threads = new Thread[treadsNum];
+        int chunkSize = fieldLenght / treadsNum;
+
+        for (int t = 0; t < steps; t++) {
+            for (int i = 0; i < treadsNum; i++) {
+                final int startInd = i * chunkSize;
+                final int endInd = (i + 1) * chunkSize;
+
+                threads[i] = new Thread(() -> {
+                    for (int pos = startInd; pos < endInd; pos++) {
+                        if (pos != 0  && pos != (fieldLenght - 1)) {
+                            temperature[pos] = temperature[pos] + speedCoef * (temperature[pos + 1] + temperature[pos - 1] - 2 * temperature[pos]);
+                        }
+                        else if (pos == 0) {
+                            temperature[pos] = temperature[pos] + speedCoef * (temperature[pos + 1] - temperature[pos]);
+                        }
+                        else {
+                            temperature[pos] = temperature[pos] + speedCoef * (temperature[pos - 1] - temperature[pos]);
+                        }
+                    }
+                });
+            }
+
+            for (Thread thread : threads) {
+                thread.start();
+            }
+
+            try {
+                for (Thread thread : threads) {
+                    thread.join();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            for (int j = 0; j < fieldLenght; j++) {
+                System.out.printf("%6s ", decimalFormat.format(temperature[j]));
+            }
+            System.out.println();
+        }
+
+        double duration = System.currentTimeMillis() - start;
+        System.out.println("Duration: " + duration);
+        return temperature;
+    }
+}
